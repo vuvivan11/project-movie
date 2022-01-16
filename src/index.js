908529ec628dfd90df75e6025f109f89f301f028
quan.nd <vuvivan11@gmail.com>
2022-01-16 12:42:15 +0700

    them thu vien

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/umd/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from 'react-redux';
-import rootReducer from './redux';
+import rootReducer from './reducer';
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))  
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/routers/index.js
@@ -0,0 +1,9 @@
+const routesHome = [
+
+]
+
+const routesAdmin = [
+
+]
+
+
