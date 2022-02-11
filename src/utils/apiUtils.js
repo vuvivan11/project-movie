d4e1d919f19ba77fd31267b0764c3254f9ed87e7
Thien <vanthien2k14@gmai.com>
2022-02-12 01:50:08 +0700

    fix tên getItem trong Authorization

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -7,7 +7,7 @@ const api = axios.create({
 api.interceptors.request.use((config)=>{
     config.headers = {
         TokenCyberSoft:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyMCIsIkhldEhhblN0cmluZyI6IjE3LzA3LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1ODAxNjAwMDAwMCIsIm5iZiI6MTYyNjcxNDAwMCwiZXhwIjoxNjU4MTYzNjAwfQ.CyAnnc8e2Rp7YmuJCdtEj-Wp7RvlDenB9Dad6NV0R20",
-        Authorization:localStorage.getItem("UserAdmin")?  `Bearer ${JSON.parse(localStorage.getItem("UserAdmin")).accessToken}` : ""
+        Authorization:localStorage.getItem("USER_LOGIN")?  `Bearer ${JSON.parse(localStorage.getItem("USER_LOGIN")).accessToken}` : ""
     }
 
     return config
